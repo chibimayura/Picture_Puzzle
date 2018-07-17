964e1c78c3ab7a09738abf32a0e65a5e5d160c12
chibimayura <chibimayura@gmail.com>
2018-07-17 01:42:42 -0700

    fix centering of save and play btn, fixed class, id selector for the hover of the btns in search.js, refresh btn works

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,15 +1,107 @@
-var newOption;
+var newOption, newTopic, stillGif, animateGif, newDiv,newImage, newImageOptions, saveBtn, playGifPuzzleBtn;
 
-var searchRecomArray = ["Animals", "Cars", "Food", "Technology", "Memes"];
-
-var searchSelector
+var searchRecomArray = ["Select a category...", "Animals", "Cars", "Food", "Technology", "Memes"];
+var currentTopic = searchRecomArray[0];
 
 //Selects the area for the drop down option selector
 var optionSelector = $("#optionSelector");
+//Selects the area where the gifs are generated
+var generateGifSelector = $("#generatedSearch");
+var generated = false;
+var randomGifs = 0;
+
+var	queryURL = "https://api.giphy.com/v1/gifs/search?api_key=7BXBSU13rlvIsFioVjPJN1Tzd8FimNGN&limit=1000&q=";
 
 for(var i = 0; i < searchRecomArray.length; i++){
-	newOption = $("<option>").text(searchRecomArray[i]);
+	newOption = $("<option>").attr("value", searchRecomArray[i]).text(searchRecomArray[i]);
 
 	optionSelector.append(newOption);
 }
 
+
+//allows user to enter a tag to search which automatically goes into the category dropdown
+$("form").on("click", "#searchGIF",function(event){
+	event.preventDefault();
+
+	if($("input").val() != ""){
+		// push new topic into search option array
+		newTopic = $("input").val();
+		searchRecomArray.push(newTopic);
+
+		newOption = $("<option>").attr("value", newTopic).text(newTopic);
+		optionSelector.append(newOption);
+
+		currentTopic = newTopic;
+		generated = true;
+	} else if(optionSelector.val() !== searchRecomArray[0]){
+		currentTopic = optionSelector.val();
+		generated = true;
+	}else {
+		generated = false;
+	}
+
+	//resets input and option field
+	$("input").val("");
+	optionSelector.val(searchRecomArray[0]);
+	if(generated){
+		generateGiphys();
+	}
+});
+
+//When mouse enters and leaves the img in the generatedSearch
+$(document).on("mouseenter", "#gifContainer", function(){
+	$(this).children("img").attr("src", $(this).children("img").attr("data-animate"));
+	$(this).children("div").fadeIn(1000);
+	$(this).children("img").css("opacity", "0.3");
+}).on("mouseleave", "#gifContainer", function(){
+	$(this).children("img").attr("src", $(this).children("img").attr("data-still"));
+	$(this).children("div").fadeOut(10);
+	$(this).children("img").css("opacity", "1");
+});
+
+$("form").on("click", "#refresh", function(){
+	event.preventDefault();
+	if(currentTopic != searchRecomArray[i] || $("input").val() !=""){
+		generateGiphys();
+	}
+});
+
+function generateGiphys(){
+	generateGifSelector.empty();
+	$.ajax({
+		url: queryURL + currentTopic,
+		method: "GET"
+	}).then(function(response){
+		for(var i = 0; i < 3; i++){
+			newDiv = $("<div id='gifContainer'>");
+
+			//storing retrieved data response and randomizing which array in the data to get
+			randomGif = Math.floor(Math.random()*response.data.length);
+			stillGif = response.data[randomGif].images.original_still.url;
+			animateGif = response.data[randomGif].images.original.url;
+
+			//all of the image attr
+			newImage = $("<img>").attr({
+				"src" : stillGif,
+				"data-state" : "still",
+				"data-still" : stillGif,
+				"data-animate" : animateGif,
+				"id" : "gif"
+			});
+
+			//create save and play btn overlayed on the gif image, append to a div
+			saveBtn = $("<div>").attr("class", "gifBtn btn btn-dark").text("Save");
+			playGifPuzzleBtn = $("<div>").attr("class", "gifBtn btn btn-dark").text("Play")
+			newImageOptions = $("<div>").attr("class", ".gifPlayOption").append(saveBtn, playGifPuzzleBtn).hide();
+
+			newDiv.append(newImage,newImageOptions);
+
+			generateGifSelector.append(newDiv);
+
+			//fade images into the generated Search area
+			generateGifSelector.each(function(i){
+				$(this).delay(400*i).fadeIn(2000);
+			});
+		}
+	});
+}
