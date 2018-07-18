00c10ded6a935399a119b5b5ac8d324151686a7f
Sherry-W <sherry.wu1219@gmail.com>
2018-07-17 20:12:00 -0700

    when suggestion img clicked it will display at the game area

--- assets/js/suggestion.js
+++ assets/js/suggestion.js
@@ -1,42 +1,35 @@
-var suggestedArray = 
-    [
-    "../Picture_Puzzle/assets/images/numbers.jpg",
-    "../Picture_Puzzle/assets/images/Pembroke-Welsh-Corgi-MP.jpg",
-    "../Picture_Puzzle/assets/images/img_1000x600.jpg",
-    "../Picture_Puzzle/assets/images/img_800x600.jpg",
-    "../Picture_Puzzle/assets/images/earth_img_600x600.jpg",
-    "../Picture_Puzzle/assets/images/sunrise.JPG",
-    "../Picture_Puzzle/assets/images/sunset.jpg",
-    "../Picture_Puzzle/assets/images/gradient-pixel-background.jpg"
-    ]
+var suggestedArray =
+   [
+   "../Picture_Puzzle/assets/images/numbers.jpg",
+   "../Picture_Puzzle/assets/images/Pembroke-Welsh-Corgi-MP.jpg",
+   "../Picture_Puzzle/assets/images/img_1000x600.jpg",
+   "../Picture_Puzzle/assets/images/img_800x600.jpg",
+   "../Picture_Puzzle/assets/images/earth_img_600x600.jpg",
+   "../Picture_Puzzle/assets/images/sunrise.JPG",
+   "../Picture_Puzzle/assets/images/sunset.jpg",
+   "../Picture_Puzzle/assets/images/gradient-pixel-background.jpg"
+   ]
 
 for (var j = 0; j < suggestedArray.length; j++) {
-    var suggestedImg = $("<img class='thumbnail'>").attr("src", suggestedArray[j]);
-    $('#nav-suggestion').append(suggestedImg);
+   var suggestedImg = $("<img class='thumbnail'>").attr("src", suggestedArray[j]);
+   $('#nav-suggestion').append(suggestedImg);
 }
 
 $(document).on('click', '.thumbnail', function(event){
-	event.preventDefault();
-	imageSRC = $(this).attr("src");
-	$("#hiddenImg").attr("src", imageSRC);
-
-
-	clearInterval(secondInterval);
-	database.ref("/lastRecordedTime").push({
-	lastRecordedTime : minute + " : " + second
-	});
-	database.ref("/lastRecordedStep").push({
-	lastRecordedStep : stepCount
-	});
-	gameStarted = false;
-	$("#target").sortedTiles(tileCount);
-	$(".btn").show();
-	$(".delete").remove();
-
-	second = 0;
-	minute = 0;
-	$("#second").text("0" + second);
-	$("#minute").text("0" + minute);
-	stepCount = initialStep;
-	stepsText.text(stepCount);
+    event.preventDefault();
+    if (gameStarted == false){
+        tileCount = 3;
+        imageSRC = $(this).attr("src");
+        $("#hiddenImg").attr("src", imageSRC);
+        $("#target").sortedTiles(tileCount);
+        $(".btn").show();
+        $(".delete").remove();
+        second = 0;
+        minute = 0;
+        $("#second").text("0" + second);
+        $("#minute").text("0" + minute);
+        stepCount = initialStep;
+        stepsText.text(stepCount);
+    }
 });
+
