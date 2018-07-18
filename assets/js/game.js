6355bb1dd5639f953ebe2c8d56ff68709b2e08c8
Sherry-W <sherry.wu1219@gmail.com>
2018-07-17 18:48:14 -0700

    deleted click completed function

--- assets/js/game.js
+++ assets/js/game.js
@@ -236,19 +236,10 @@ function addCompleteImg() {
     }
 };
 
-$(document).on('click', '.completed', function() {
-    var modal = document.getElementById('myModal');
-    var img = document.getElementsByClassName('completed');
-    var modalImg = document.getElementById("img01");
-    img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    }
-});
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function() { 
-    modal.style.display = "none";
-}
+// $(document).on('click', '.completed', function() {
+
+    
+// });
 
 
 database.ref("/stepRecord").on("value", function(snapshot){
