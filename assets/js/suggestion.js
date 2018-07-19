4d286d7d88b17a17fb8f4c99896d83d4f89650e3
Sherry-W <sherry.wu1219@gmail.com>
2018-07-19 04:30:27 -0700

    modal effect on completed images
    Things got updated 7/19/2018:
    
    ##HTML:
    
    added the modal container/effect under [id="nav-completed"]
    
    ##CSS:
    
    added the modal related styles
    changed #hiddenImg - display: hidden to none.
    	-everytime when refreshing the page, you will see the img blinked
    
    ##SUGGESTION.JS
    
    added images for 9/16/25/36 pieces
    added function for the '.play' to display on game area
    added one-click-download function
    	**one thing tho.. it only works on the images we have in the image folder. for the giphys, it will open as a webpage instead of downloading the image.
    	https://www.chromestatus.com/feature/4969697975992320
    	many browsers blocked cross-origin <a download>

--- assets/js/suggestion.js
+++ assets/js/suggestion.js
@@ -1,6 +1,9 @@
 var suggestedArray =
    [
-   "../Picture_Puzzle/assets/images/numbers.jpg",
+   "../Picture_Puzzle/assets/images/numbers_9_pieces.jpg",
+   "../Picture_Puzzle/assets/images/numbers_16_pieces.jpg",
+   "../Picture_Puzzle/assets/images/numbers_25_pieces.jpg",
+   "../Picture_Puzzle/assets/images/numbers_36_pieces.jpg",
    "../Picture_Puzzle/assets/images/Pembroke-Welsh-Corgi-MP.jpg",
    "../Picture_Puzzle/assets/images/img_1000x600.jpg",
    "../Picture_Puzzle/assets/images/img_800x600.jpg",
@@ -33,3 +36,47 @@ $(document).on('click', '.thumbnail', function(event){
     }
 });
 
+$(document).on('click', '.play', function(event){
+    event.preventDefault();
+    if (gameStarted == false){
+        tileCount = 3;
+        imageSRC = $(this).parent().siblings().attr("data-still");
+        console.log(imageSRC);
+        $("#hiddenImg").attr("src", imageSRC);
+        $("#target").sortedTiles(tileCount);
+        $(".btn").show();
+        $(".delete").remove();
+        second = 0;
+        minute = 0;
+        $("#second").text("0" + second);
+        $("#minute").text("0" + minute);
+        stepCount = initialStep;
+        stepsText.text(stepCount);
+    }
+});
+
+$(document).on('click', '.completed', function() {
+    // Get the modal
+    var modal = document.querySelector('#containerForDisplay');
+
+    // Get the image and insert it inside the modal 
+    var img = document.querySelector('.completed');
+    var modalImg = document.querySelector("#displayFullSize");
+   
+    modal.style.display = "block";
+    modalImg.src = this.src;
+
+    $('#download').empty();
+    var download = $("<a download target='_blank'>").attr('href', $(this).attr('src'));
+    var downloadButton = $('<button>').text('Download Image');
+    download.append(downloadButton);
+    $('#download').append(download);
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+        modal.style.display = "none";
+    }
+});
